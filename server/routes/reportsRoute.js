a26427bf92550b5b1417fee559f01e0488bda0bc
KenanAkbarli <tu6m5m78u@code.edu.az>
2023-02-18 23:46:01 +0400

    feat:add get all attempts api in reportRoute

--- server/routes/reportsRoute.js
+++ server/routes/reportsRoute.js
@@ -1,22 +1,42 @@
-const authMiddleware = require('../middlewares/authMiddleware';)
+const authMiddleware = require("../middlewares/authMiddleware");
+const Exam = require("../models/examModel");
+const User = require("../models/userModel");
+const router = require("express").Router();
 
-const router = require('express').Router();
+// ADD REPORT
 
-// ADD ATTEMPT
+router.post("/add-report", authMiddleware, async (req, res) => {
+  try {
+    const newReport = new Report(req.body);
+    await newReport.save();
+    res.send({
+      message: "Nəticə əlavə edildi",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
 
-router.post('/add-report',authMiddleware,  async (req, res)=> {
-    try {
-        const newReport = new Report(req.body);
-        await newReport.save();
-        res.send({
-            message: 'Nəticə əlavə edildi',
-            success: true,
-        })
-    } catch (error) {
-       res.status(500).send({
-        message: error.message,
-        data: error,
-        success: false
-       })
-    }
-})
+// GET ALL ATTEMPTS
+
+router.post("/get-all-attempts", authMiddleware, async (req, res) => {
+  try {
+    const reports = await Report.find();
+    res.send({
+      message: "Nəticə gətrildi",
+      data: reports,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
