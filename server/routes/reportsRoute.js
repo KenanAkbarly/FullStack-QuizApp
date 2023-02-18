7d7fe0052389bb28a7783568e8d7e1718f90b30b
KenanAkbarli <tu6m5m78u@code.edu.az>
2023-02-18 23:54:37 +0400

    feat:add get all reports by user api in reportRoute

--- server/routes/reportsRoute.js
+++ server/routes/reportsRoute.js
@@ -1,6 +1,7 @@
 const authMiddleware = require("../middlewares/authMiddleware");
 const Exam = require("../models/examModel");
 const User = require("../models/userModel");
+const Report = require("../models/reportModel");
 const router = require("express").Router();
 
 // ADD REPORT
@@ -22,11 +23,30 @@ router.post("/add-report", authMiddleware, async (req, res) => {
   }
 });
 
-// GET ALL ATTEMPTS
+// GET ALL REPORTS
 
 router.post("/get-all-attempts", authMiddleware, async (req, res) => {
   try {
     const reports = await Report.find();
+    res.send({
+      message: "Nəticə gətrildi",
+      data: reports,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+
+// GET ALL REPORTS BY USER
+
+router.post("/get-all-attempts-by-user", authMiddleware, async (req, res) => {
+  try {
+    const reports = await Report.find({ user: req.body.userId });
     res.send({
       message: "Nəticə gətrildi",
       data: reports,
