b1a5bb95eb64713921654ae31a5d3864aa050fa0
KenanAkbarli <tu6m5m78u@code.edu.az>
2023-02-27 02:48:41 +0400

    feat:add carusel in home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import HomeHeader from '../../components/HomeHeader'
 import styled from './style.module.scss'
+import Carusel from '../../components/Carusel';
 const Home = () => {
   return (
     <div >
@@ -36,26 +37,8 @@ const Home = () => {
 
         </div>
       </div>
-    <div>
-    <Carousel>
-                <div className={styled.carusel_div}>
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate explicabo esse odit quod, aspernatur, possimus provident dolor quisquam magnam maiores est unde atque, nemo perferendis voluptatem labore accusantium necessitatibus consectetur exercitationem vel alias harum nisi. Earum suscipit illum cumque eligendi numquam quibusdam id fugit! Pariatur natus ex voluptatum facere esse impedit consequatur praesentium, soluta porro recusandae tenetur mollitia, reiciendis minima ipsam officiis explicabo suscipit illum dolorem voluptate eaque consectetur? Corrupti maiores dolorem assumenda vitae sunt voluptatum nulla, earum harum, explicabo esse fuga! Ducimus esse, iste harum minima nam aperiam amet itaque delectus in omnis, deserunt praesentium ipsam sequi debitis repellat.
-                   </div>
-                    <div className={styled.image_body}>
-                    <img src="https://cdn.theatlantic.com/media/img/photo/2018/10/images-of-the-season-fall-is-in-the/f02_RTX6EJJJ-1/original.jpg" />
-                    </div>
-                
-                </div>
-                <div>
-                    <img src="https://cdn.theatlantic.com/media/img/photo/2018/10/images-of-the-season-fall-is-in-the/f02_RTX6EJJJ-1/original.jpg" />
-                    
-                </div>
-                <div>
-                    <img src="https://cdn.theatlantic.com/media/img/photo/2018/10/images-of-the-season-fall-is-in-the/f02_RTX6EJJJ-1/original.jpg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
+    <div className={styled.carusel_body}>
+      <Carusel/>
     </div>
     </section>
     </div>
