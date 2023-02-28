ddf1c57e9f68e82880d9914665f7c4043250a3f4
KenanAkbarli <tu6m5m78u@code.edu.az>
2023-03-01 00:14:03 +0400

    feat:update dark mode switch

--- client/src/components/Switch/index.jsx
+++ client/src/components/Switch/index.jsx
@@ -10,21 +10,24 @@ const Switchh = () => {
     <div>
         <div
     className="switch"
-    onClick={()=> dispatch(toggleDarkMode())}
+    
   >
-     <img
-      className="switch__moon"
-      src="	https://dev-jobs-site.netlify.app/assets/desktop/icon-moon.svg"
-      alt="moon"
-    />
-    <div >
-      <div className="switch__root__circle" />
-      <Switch color="default"  defaultChecked />
-    </div>
      <img
       className="switch__sun"
       src="https://dev-jobs-site.netlify.app/assets/desktop/icon-sun.svg"
       alt="sun"
+    />
+    
+    <div >
+      <div className="switch__root__circle" />
+      <Switch
+      onClick={()=> dispatch(toggleDarkMode())}
+      color="default"  defaultChecked />
+    </div>
+    <img
+      className="switch__moon"
+      src="	https://dev-jobs-site.netlify.app/assets/desktop/icon-moon.svg"
+      alt="moon"
     />
   </div>
     </div>
